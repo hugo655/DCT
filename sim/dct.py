a31e395cd9050c7a792c017cfe45188e2bb335b9
hugo.medeiros <hugo.medeiros@localhost.localdomain>
2021-05-10 09:00:39 -0300

    Add python simulation files

---
+++ sim/dct.py
@@ -0,0 +1,18 @@
+#!/usr/bin/ python3
+#######################
+# Author: Hugo Gayoso #
+# git: hugo655        #
+#######################
+import pathlib
+import numpy as np
+
+path =str(pathlib.Path(__file__).parent.absolute())
+print("Running dct.py in " + path +" ...")
+
+A = np.array([[5,10,20,0]]).T
+Cd = np.array([[1,1,1,1],[2,1,-1,-2],[1,-1,-1,1],[1,-2,2,-1]])
+Ci = np.array([[1,1,1,1/2],[1,1/2,-1,-1],[1,-1/2,-1,1],[1,-2,1,-1/2]])
+Re10 = np.array([[1/4,0,0,0],[0,1/5,0,0],[0,0,1/4,0],[0,0,0,1/5]])
+
+P = np.dot(A.T,A)
+print(Ci@Cd@A)
